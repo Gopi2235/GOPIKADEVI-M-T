720a02a35ed27db0b41221f6a13424ee43271607
Gopi2235 <118247460+Gopi2235@users.noreply.github.com>
2023-12-13 13:42:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import bus from './Components/Assets/bus.jpg'
+import './App.css'
+import LoginSignUp from './Components/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+    <div >
+        <LoginSignUp/>
+    </div>
+  );
+}
+
+export default App;
